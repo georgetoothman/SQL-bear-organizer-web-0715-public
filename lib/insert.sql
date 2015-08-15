fef96713827e03c01f6de7be5130b508fe9b1f22
George Toothman <george.toothman@flatironschool.com>
2015-08-15 11:26:35 -0400

    add select and insert queries

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Mr. Chocolate", 1, "m", "green", "hungry", 1),
+        ("Rowdy", 2, "f", "green", "hungry", 1),
+        ("Tabitha", 3, "m", "green", "hungry", 1),
+        ("Sergeant Brown", 4, "f", "green", "hungry", 1),
+        ("Melissa", 5, "m", "green", "hungry", 1),
+        ("Grinch", 6, "f", "green", "hungry", 1),
+        ("Wendy", 7, "m", "green", "hungry", 1),
+        (NULL, 8, "f", "green", "hungry", 1);
